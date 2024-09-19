a99183dbf91e049d436160d08f38af003df928f9
Ayush Acharjya <ayushacharjya@gmail.com>
2024-09-19 21:38:00 +0100

    add NEXT_PUBLIC_API_BASE_URL as dependency in next.config.mjs

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-      NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
-    },
-  };
-  
-  export default nextConfig;
-  
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  },
+};
+
+export default nextConfig;
